007400bb5053d5043e702d4b6c3888de09a7d229
mohammadraziei <mohammadraziei1375@gmail.com>
2024-04-24 21:16:01 +0330

    try to fix python project for torch

--- src/binding/bind.cpp
+++ src/binding/bind.cpp
@@ -1,13 +1,13 @@
-#include "torch.h"
+#include "lltm.h"
 #include <pybind11/pybind11.h>
 
 PYBIND11_MODULE(TORCH_EXTENSION, m) {
-//m.def("forward", &lltm_forward, "LLTM forward");
-//m.def("backward", &lltm_backward, "LLTM backward");
+    m.def("forward", &lltm_forward, "LLTM forward");
+    m.def("backward", &lltm_backward, "LLTM backward");
 #ifdef VERSION_INFO
-m.attr("__version__") = MACRO_STRINGIFY(VERSION_INFO);
+    m.attr("__version__") = MACRO_STRINGIFY(VERSION_INFO);
 #else
-m.attr("__version__") = "dev";
+    m.attr("__version__") = "dev";
 #endif
 }
 
